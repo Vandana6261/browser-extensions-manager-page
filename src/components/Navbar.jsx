90e2537aef5565ecc0388d46efdbed865493e788
Abhishek-choudhary1001 <abhishekcool301@gmail.com>
2025-10-04 12:46:30 +0530

    working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import sun  from '../assets/images/icon-sun.svg'
 function Navbar() {
   return (
     <div className=' max-w-[1200px] mx-auto  bg-[hsl(226,25%,17%)] '>
-        <header className='flex justify-between py-2 px-4 bg-white rounded-md sticky top-0 left-1/2 -translate-x-1/2 min-w-[85%] '>
+        <header className='flex justify-between py-2 px-4 bg-white rounded-md fixed top-0 left-1/2 -translate-x-1/2 min-w-[85%] '>
             <img className='' src={logo} alt="Logo" /> 
             <div className='bg-[hsl(225,23%,24%)] w-10 h-10 flex justify-center items-center rounded-md'>
               <img src={sun} alt="Sun" className='object-fit' />
